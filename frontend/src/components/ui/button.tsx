4e000925f82dbcbfad29993e4d45743e5d1031a7
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2025-03-19 11:19:49 +0700

    expose buttonprops for chatbubble

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -32,19 +32,15 @@ const buttonVariants = cva(
   }
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+function Button({ className, variant, size, asChild = false, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button'
 
   return <Comp data-slot='button' className={cn(buttonVariants({ variant, size, className }))} {...props} />
 }
 
-export { Button, buttonVariants }
+type ButtonProps = React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }
+
+export { Button, buttonVariants, type ButtonProps }
